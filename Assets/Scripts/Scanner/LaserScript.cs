237a8cf09c1e1190dcdd58d94207da6cf63aee19
10dimensions <relay.10d@gmail.com>
2021-01-12 21:30:42 +0530

    Alter color based on range

--- Assets/Scripts/Scanner/LaserScript.cs
+++ Assets/Scripts/Scanner/LaserScript.cs
@@ -4,7 +4,7 @@
  public class LaserScript : MonoBehaviour
  {
      public LineRenderer laserLineRenderer;
-     private float laserWidth = 0.25f;
+     private float laserWidth = 0.5f;
 	 
      [SerializeField] private float laserMaxLength; 
 	 [SerializeField] private float angularOffset;
@@ -16,33 +16,45 @@
         laserLineRenderer.SetPositions( initLaserPositions );
         laserLineRenderer.SetWidth( laserWidth, laserWidth );
 		 
-		StartCoroutine( ShootLaserFromTargetPosition( CarBase.position, laserMaxLength ) );  
+		StartCoroutine( ProjectLaser( CarBase.position, laserMaxLength ) );  
      }
 		
 	//Set Laser Parameters Length, & Angle
-     void SetParameters (float _length, float _angle) 
+     public void SetParameters (float _length, float _angle) 
      {	
 		laserMaxLength = _length;
 		angularOffset = _angle;
      }
- 
-     IEnumerator ShootLaserFromTargetPosition( Vector3 targetPosition, float length )
+		
+		
+     IEnumerator ProjectLaser( Vector3 targetPosition, float length )
      {	
-		Vector3 direction = Quaternion.Euler(0, angularOffset, 0) * CarBase.forward;
+		 Vector3 direction = Quaternion.Euler(0, angularOffset, 0) * CarBase.forward;	//Offset Ray Angle
+		
          Ray ray = new Ray( targetPosition, direction );
          RaycastHit raycastHit;
          Vector3 endPosition = targetPosition + ( length * direction );
- 
+			
+		  //Raycast from Base
          if( Physics.Raycast( ray, out raycastHit, length ) ) {
              endPosition = raycastHit.point;
          }
- 
+		 
+		 //Redraw Laser
          laserLineRenderer.SetPosition( 0, targetPosition );
          laserLineRenderer.SetPosition( 1, endPosition );
+		 		 
+		// Change color based on normalized distance (0 = red, 1 = green)
+		float lineColorLength = (endPosition - targetPosition).magnitude / laserMaxLength;
+		Color _color = Color.Lerp(Color.red, Color.green, lineColorLength);
+		laserLineRenderer.material.color = _color;
+		//laserLineRenderer.endColor = _color;
+		
 		 
 		 yield return new WaitForSeconds(0.25f);
 		 
-		 StartCoroutine( ShootLaserFromTargetPosition( transform.parent.position, laserMaxLength ) ); 
+		 StartCoroutine( ProjectLaser( transform.parent.position, laserMaxLength ) ); 
 		 
      }
+
  }
--- Assets/Scripts/Scanner/Scanner.cs
+++ Assets/Scripts/Scanner/Scanner.cs
@@ -16,10 +16,10 @@
 		
 		for(int i=0; i<360; i=i+(360/FOV)){
 			
-			Debug.Log(i);
-			
+			//Instantiate Laser Node Object
 			GameObject _laserNode = Instantiate(LaserNodeRef, this.transform.position, this.transform.rotation) as GameObject;
 			_laserNode.transform.parent = this.transform;
+			_laserNode.GetComponent<LaserScript>().SetParameters(LaserLength, i);	//Set Parameters for Laser Object
 			_laserNode.SetActive(true);
 			
 		}
