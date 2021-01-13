066f115f7b66a90438b56017969ccfb2e6729560
10dimensions <relay.10d@gmail.com>
2021-01-13 14:56:25 +0530

    Implement RGB Camera as RenderTexture

---
+++ Assets/Scripts/Controller/CameraArray.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraArray : MonoBehaviour
+{	
+	[SerializeField] private RenderTexture[] CameraArrayStreams;	//0-rgb	1-depth	2-segmentation
+	private Camera cam;
+
+    private void Awake()
+	{
+		cam = GetComponent<Camera>();
+	}
+
+	private void Start()
+	{
+		StartCoroutine(RenderTexturesCoroutine());
+	}
+
+	IEnumerator RenderTexturesCoroutine()
+	{
+		cam.targetTexture = CameraArrayStreams[0];
+		yield return new WaitForSeconds(1f);
+		cam.Render();
+	}
+
+}
