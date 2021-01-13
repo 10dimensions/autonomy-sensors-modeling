ba4c6114c7ce22fa4b2d7d19ab687659ddfdd6f0
10dimensions <relay.10d@gmail.com>
2021-01-13 17:29:21 +0530

    Access depth buffer

--- Assets/Scripts/Controller/CameraArray.cs
+++ Assets/Scripts/Controller/CameraArray.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CameraArray : MonoBehaviour
 {	
 	[SerializeField] private RenderTexture[] CameraArrayStreams;	//0-rgb	1-depth	2-segmentation
+	[SerializeField] private Material DepthMapMat;
 	private Camera cam;
 
     private void Awake()
 	{
 		cam = GetComponent<Camera>();
+		cam.depthTextureMode = DepthTextureMode.Depth;
 	}
 
 	private void Start()
@@ -18,10 +20,21 @@ public class CameraArray : MonoBehaviour
 	}
 
 	IEnumerator RenderTexturesCoroutine()
-	{
+	{	
 		cam.targetTexture = CameraArrayStreams[0];
+ 
+		Graphics.Blit(CameraArrayStreams[0], CameraArrayStreams[1], DepthMapMat);
+		//cam.SetTargetBuffers (CameraArrayStreams[1].depthBuffer, CameraArrayStreams[0].colorBuffer);
+		
 		yield return new WaitForSeconds(1f);
 		cam.Render();
+		
 	}
+	
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {	
+		Graphics.Blit(source, destination, DepthMapMat);
+        //Graphics.Blit(source, destination, mat);
+    }
 
 }
